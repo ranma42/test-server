9f0875f92fd5343cc36d2510d7ef7fd221f10d36
Amir Hardon <amirha@google.com>
2025-03-22 20:09:38 +0000

    Some cleanup

--- internal/record/recording_https_proxy.go
+++ internal/record/recording_https_proxy.go
@@ -1,8 +1,10 @@
 package record
 
 import (
+	"bytes"
 	"encoding/hex"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -38,28 +40,78 @@ func (r *RecordingHTTPSProxy) Start() error {
 }
 
 func (r *RecordingHTTPSProxy) handleRequest(w http.ResponseWriter, req *http.Request) {
+	err := r.recordRequest(req)
+	if err != nil {
+		fmt.Printf("Error recording request: %v\n", err)
+		http.Error(w, fmt.Sprintf("Error recording request: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	_, _, err = r.proxyRequest(w, req)
+	if err != nil {
+		fmt.Printf("Error proxying request: %v\n", err)
+		http.Error(w, fmt.Sprintf("Error proxying request: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (r *RecordingHTTPSProxy) recordRequest(req *http.Request) error {
 	recordedRequest, err := store.NewRecordedRequest(req, r.prevRequestSHA, *r.config)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	recordedRequest.RedactHeaders(r.config.RedactRequestHeaders)
 
 	reqHash, err := recordedRequest.ComputeSum()
 	if err != nil {
-		panic(err)
+		return err
 	}
 	reqHashHex := hex.EncodeToString(reqHash[:])
-	s := recordedRequest.Serialize()
 
 	recordPath := filepath.Join(r.recordingDir, reqHashHex+".req")
 	err = os.WriteFile(recordPath, []byte(recordedRequest.Serialize()), 0644)
 	if err != nil {
-		fmt.Printf("Error saving request to file: %v\n", err)
+		return err
+	}
+	return nil
+}
+
+func (r *RecordingHTTPSProxy) proxyRequest(w http.ResponseWriter, req *http.Request) (*http.Response, []byte, error) {
+	url := fmt.Sprintf("https://%s:%d%s", r.config.TargetHost, r.config.TargetPort, req.URL.Path)
+	if req.URL.RawQuery != "" {
+		url += "?" + req.URL.RawQuery
 	}
 
-	fmt.Printf("%v", s)
-	// TODO: Implement the request handling logic here
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hello, world!"))
+	bodyBytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.Body.Close()
+
+	proxyReq, err := http.NewRequest(req.Method, url, bytes.NewReader(bodyBytes))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(proxyReq)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	respBodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	w.Write(respBodyBytes) // Send original (compressed) body to client
+	return resp, respBodyBytes, nil
 }
